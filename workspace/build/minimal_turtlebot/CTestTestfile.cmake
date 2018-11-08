3060a6903935b14243e8cbb95ddbe53a51671b37
axn337 <axn337@case.edu>
2018-11-08 16:07:35 -0500

    Turtlebot Setup Complete

---
+++ workspace/build/minimal_turtlebot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/Desktop/MailBot/workspace/src/minimal_turtlebot
+# Build directory: /home/user/Desktop/MailBot/workspace/build/minimal_turtlebot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
