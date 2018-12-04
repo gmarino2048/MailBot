9b5fd727a4a5b44bbb4f2b69d74358edb785a56f
Guy Marino <gxm262@case.edu>
2018-12-04 15:02:47 -0500

    ;sdlkfj

--- workspace/src/minimal_turtlebot/src/src/reaction.cpp
+++ workspace/src/minimal_turtlebot/src/src/reaction.cpp
@@ -15,7 +15,7 @@ namespace Reaction
         uint8_t next_state = Default;
         Turtlebot_Reaction reaction;
 
-        cout << current_time;
+        std::cout << current_time;
 
         // State machine should always stop if lifted
         if (sensors.WHEEL != 0)
